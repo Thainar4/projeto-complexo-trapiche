17f7ef0f8e45e8a55e155130e337c15894e904f9
Luís Abreu <lwiiszkx@gmail.com>
2026-06-01 11:24:40 -0300

    Finalização Transições

--- Assets/Scipts/MainMenu.cs
+++ Assets/Scipts/MainMenu.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 #if UNITY_EDITOR
@@ -10,11 +11,42 @@ public class MainMenu : MonoBehaviour
     [Header("Painel de Créditos")]
     public GameObject painelCreditos;
 
+    [Header("Cena Principal")]
+    public string nomeDaCenaPrincipal = "Praça";
+    public string nomeDoSpawnDestino = "SpawnPlayer";
+
+    [Header("Input VR")]
+    public Key teclaTeste = Key.Enter;
+
+    private bool estaCarregando;
+    private bool botaoAEstavaPressionado;
+
+    private void Update()
+    {
+        if (estaCarregando || painelCreditos != null && painelCreditos.activeSelf)
+            return;
+
+        if (ApertouConfirmar())
+            IniciarTour();
+    }
+
     public void IniciarTour()
     {
+        if (estaCarregando)
+            return;
+
+        estaCarregando = true;
+
         Debug.Log("Botão INICIAR TOUR foi clicado!");
 
-        SceneManager.LoadScene("Praça");
+        if (SceneTransitionManager.Instance != null)
+        {
+            SceneTransitionManager.Instance.LoadScene(nomeDaCenaPrincipal, nomeDoSpawnDestino);
+            return;
+        }
+
+        PlayerSpawnManager.spawnDestino = nomeDoSpawnDestino;
+        SceneManager.LoadScene(nomeDaCenaPrincipal);
     }
 
     public void AbrirCreditos()
@@ -40,5 +72,32 @@ public class MainMenu : MonoBehaviour
         #else
         Application.Quit();
         #endif
+    }
+
+    private bool ApertouConfirmar()
+    {
+        bool apertouTeclado = Keyboard.current != null &&
+                              Keyboard.current[teclaTeste].wasPressedThisFrame;
+
+        bool apertouGamepad = Gamepad.current != null &&
+                              Gamepad.current.buttonSouth.wasPressedThisFrame;
+
+        bool botaoAAtual = false;
+
+        UnityEngine.XR.InputDevice controleDireito =
+            UnityEngine.XR.InputDevices.GetDeviceAtXRNode(UnityEngine.XR.XRNode.RightHand);
+
+        if (controleDireito.isValid)
+        {
+            controleDireito.TryGetFeatureValue(
+                UnityEngine.XR.CommonUsages.primaryButton,
+                out botaoAAtual
+            );
+        }
+
+        bool apertouBotaoAQuest = botaoAAtual && !botaoAEstavaPressionado;
+        botaoAEstavaPressionado = botaoAAtual;
+
+        return apertouTeclado || apertouGamepad || apertouBotaoAQuest;
     }
 }
